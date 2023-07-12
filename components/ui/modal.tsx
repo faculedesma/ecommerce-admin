71607db81a6648b8ffe929c62b38e805a08ce8e1
Facundo Ledesma <facundo.ledesma@Facundos-MBP.wind3.hub>
2023-07-12 02:02:39 +0200

    feat: add global modal component

---
+++ components/ui/modal.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from "./dialog";
+
+interface IModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+export const Modal: React.FC<IModalProps> = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+          <div>{children}</div>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
